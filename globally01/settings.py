d5e7c4358102412509d1da94fa30e87c8ffba6d4
tanvir <md.tanvirahmed217@gmai.com>
2024-12-15 11:16:42 +0600

    initial commit

--- globally01/settings.py
+++ globally01/settings.py
@@ -69,8 +69,13 @@ WSGI_APPLICATION = 'globally01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb1',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
     }
 }
 
